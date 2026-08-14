345a174905f825b2bea3295916b01e453972770b
Muhammad Ahsan <smartsoloutionservicess@gmail.com>
2026-08-15 00:04:18 +0500

    Use complete App Builder v2

--- src/routes/_authenticated/admin.app-customization.tsx
+++ src/routes/_authenticated/admin.app-customization.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { AppBuilder } from '@/components/customization/AppBuilder';
+import { AppBuilderV2 } from '@/components/customization/AppBuilderV2';
 
 export const Route = createFileRoute('/_authenticated/admin/app-customization')({ component: AppCustomization });
 
 function AppCustomization() {
-  return <AppBuilder />;
+  return <AppBuilderV2 />;
 }
