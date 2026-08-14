67bd0aa51795e9f622589b435957e9b40428a5dc
Muhammad Ahsan <smartsoloutionservicess@gmail.com>
2026-08-15 00:25:43 +0500

    Wire Customization Studio directly to Builder V2

--- src/routes/_authenticated/admin.app-customization.tsx
+++ src/routes/_authenticated/admin.app-customization.tsx
@@ -4,5 +4,9 @@ import { AppBuilderV2 } from '@/components/customization/AppBuilderV2';
 export const Route = createFileRoute('/_authenticated/admin/app-customization')({ component: AppCustomization });
 
 function AppCustomization() {
-  return <AppBuilderV2 />;
+  return (
+    <div data-customization-studio-version="builder-v2" className="h-full min-h-0">
+      <AppBuilderV2 />
+    </div>
+  );
 }
